233a0d3416a1ef6310c5c67f4393214f4bdd96c0
fr4n3k <franekj@gmail.com>
2020-12-08 11:18:55 +0100

    add figure size set tp (12.5, 8)

--- rw_visual.py
+++ rw_visual.py
@@ -10,7 +10,7 @@ while True:
 
     # Plot the points in the walk.
     plt.style.use('classic')
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(12.5, 8))
     point_numbers = range(rw.num_points)
     ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
                edgecolors='none', s=1)
